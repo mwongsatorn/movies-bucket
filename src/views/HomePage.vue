277fc196244b213f2f82b2f77497c30e2f084db9
Wongsatorn Pornpirunroch <wongsatorn.fw@gmail.com>
2022-12-09 12:35:53 +0700

    feat: fetch popular movies/series data on homepage

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,4 +1,26 @@
 <script setup>
+import MovieCard from "../components/MovieCard.vue";
+import { ref } from "vue";
+
+const popularMovies = ref(null);
+const popularSeries = ref(null);
+
+try {
+  const moviesRes = await fetch(
+    "https://api.themoviedb.org/3/movie/popular?api_key=f3764e9b52bcedab6431af751cd6106a&language=en-US&page=1"
+  );
+  const { results: moviesResults } = await moviesRes.json();
+  popularMovies.value = moviesResults;
+
+  const seriesRes = await fetch(
+    "https://api.themoviedb.org/3/tv/popular?api_key=f3764e9b52bcedab6431af751cd6106a&language=en-US&page=1"
+  );
+  const { results: seriesResults } = await seriesRes.json();
+  popularSeries.value = seriesResults;
+} catch (e) {
+  console.log(e);
+}
+
 function handleListScroll({ target }) {
   const { scrollLeft, nextElementSibling } = target;
   if (scrollLeft >= 10) {
@@ -18,7 +40,15 @@ function handleListScroll({ target }) {
           <div
             @scroll="handleListScroll"
             class="mt-8 flex gap-4 overflow-x-auto pb-12"
-          ></div>
+          >
+            <MovieCard
+              v-for="movie in popularMovies"
+              :key="movie.id"
+              :title="movie.title"
+              :vote_average="movie.vote_average"
+              :poster_path="movie.poster_path"
+            />
+          </div>
           <div
             class="pointer-events-none absolute top-0 right-0 h-[calc(100%-2rem)] w-[20%] bg-gradient-to-r from-transparent to-white transition"
           ></div>
@@ -32,7 +62,15 @@ function handleListScroll({ target }) {
           <div
             @scroll="handleListScroll"
             class="mt-8 flex gap-4 overflow-x-auto pb-12"
-          ></div>
+          >
+            <MovieCard
+              v-for="movie in popularSeries"
+              :key="movie.id"
+              :title="movie.name"
+              :vote_average="movie.vote_average"
+              :poster_path="movie.poster_path"
+            />
+          </div>
           <div
             class="pointer-events-none absolute top-0 right-0 h-[calc(100%-2rem)] w-[20%] bg-gradient-to-r from-transparent to-white transition"
           ></div>
