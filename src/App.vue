73b5857d016ce73ec5782952d2514446c497045a
Wongsatorn Pornpirunroch <wongsatorn.fw@gmail.com>
2022-12-09 12:40:54 +0700

    feat: make root app handle async dependecies

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,24 @@
 <script setup>
-import { RouterLink, RouterView } from "vue-router";
+import { RouterView } from "vue-router";
 import AppHeader from "./components/AppHeader.vue";
 </script>
 
 <template>
   <AppHeader></AppHeader>
-  <RouterView></RouterView>
+  <RouterView v-slot="{ Component }">
+    <Suspense timeout="1000">
+      <template #default>
+        <component :is="Component" />
+      </template>
+      <template #fallback>
+        <div
+          class="flex h-[calc(100vh-68px)] items-center justify-center text-7xl font-bold"
+        >
+          Loading...
+        </div>
+      </template>
+    </Suspense>
+  </RouterView>
 </template>
 
 <style scoped></style>
